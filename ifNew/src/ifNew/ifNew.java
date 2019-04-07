31bdf4e163c9fd6848dd6bddd905978729eafba4
salah224 <salah224@hotmail.com>
2019-04-07 08:58:50 -0500

    if statement

---
+++ ifNew/src/ifNew/ifNew.java
@@ -0,0 +1,30 @@
+package ifNew;
+
+public class ifNew {
+
+	public ifNew() {
+		
+	}
+	public static void main(String[] args) {
+		
+		int temp = 70;
+		
+		if(temp == 100){
+			
+		System.out.println(" the Temp = 100");
+		}
+		
+		if(temp < 100){
+				System.out.println("the temp is less the 100");
+	}
+		
+		if(temp <= 50 ){
+			System.out.println("the Temp is %50 or less");
+		}
+		if(temp > 100){
+			System.out.println("the Temp over heat");
+			
+		}
+	
+	}
+}
